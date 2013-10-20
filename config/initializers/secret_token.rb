bd89d571993ae24be33eaed3630b5aecb676b136
Razix <uscreamrazix@gmail.com>
2013-10-20 12:35:56 +0300

    f

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogbuilder::Application.config.secret_token = '4a5ca429d0a16cfffbdcef5b096d8a1a337a85412d909fc534ddd5391232cafb4d912db13438ed5cd0ddb64da8712a47b8557068db57055f6bd67f0d4cfb3753'
